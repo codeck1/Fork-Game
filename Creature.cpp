837676c07c2ef7012484c8625f12fecadb3a5cd8
codeck1 <marc.mensajes@gmail.com>
2016-11-08 10:56:38 +0100

    Implemented Player.h and Player.cpp in progress

---
+++ Creature.cpp
@@ -0,0 +1,27 @@
+#include "Creature.h"
+
+Creature::Creature(const char * name, const char * description, Room * room) : Entity(name, description, (Entity*)room)
+{
+	type = CREATURE;
+}
+
+Creature::~Creature()
+{
+}
+
+void Creature::Look(const vector<string>& args)
+{
+	if (isAlive)
+	{
+		cout << name << "\n";
+		cout << description << "\n";
+	}
+	else
+	{
+		cout << name << "\n";
+		cout << description << "Completely dead. No doubt about that.";
+
+	}
+}
+
+
---
+++ Creature.h
@@ -0,0 +1,27 @@
+#ifndef _CREATURE_
+#define _CREATURE_
+
+//Include
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Room.h"
+
+class Room;
+
+using namespace std;
+
+class Creature : public Entity
+{
+public:
+	Creature(const char* name, const char* description, Room* room);
+	~Creature();
+
+	virtual void Look(const vector<string>& args);
+
+public:
+	bool isAlive;
+
+};
+
+#endif //__Creature__
